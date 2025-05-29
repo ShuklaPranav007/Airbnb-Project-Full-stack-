5b8b1ed4a6a970ba3cf77fa21ead688489e36d19
ShuklaPranav007 <shuklapranav739@gmail.com>
2025-05-29 23:20:53 +0530

    signup

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExoressError.js")
 const session = require("express-session")
 const flash = require("connect-flash")
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js")
 
-const listings = require("./routes/listing.js");
-const review = require('./routes/review.js');
-const Review = require("./models/review.js");
-const { date } = require("joi");
+const listingsRouter = require("./routes/listing.js");
+const reviewRouter = require('./routes/review.js');
+const userRouter = require("./routes/user.js");
+
 
 
 main()
@@ -32,13 +35,13 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 
 
 const sessionOptions = {
-  secret : "mysecret",
+  secret: "mysecret",
   resave: false,
   saveUninitialized: true,
-  cookie :{
-    expires : Date.now() +7* 24* 60* 60* 1000,
-    maxAge :7* 24* 60* 60* 1000,
-    httpOnly : true
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true
   },
 };
 
@@ -51,19 +54,34 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
-app.use((req, res, next)=>{
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// middleware
+app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 })
 
-
-
+// app.get("/demouser", async (req, res) => {
+//   let fakeUser = new User({
+//     email : "pranav@gmail.com",
+//     username :  "pranav007"
+//   })
+//   let registeredUser = await User.register(fakeUser, "abcd");
+//   res.send(registeredUser);
+// })
 
 // routes
-app.use("/listings", listings);
-app.use('/listings/:id/reviews', Review);
-
+app.use("/listings", listingsRouter);
+app.use('/listings/:id/reviews', reviewRouter);
+app.use('/', userRouter);
 
 
 app.all("*", (req, res, next) => {
